85df7c8261e2d17afdf720465c1a68f4e1760354
Nick lang <nll5171@rit.edu>
2024-12-04 18:46:14 -0500

    Changed requires for index.js to make it stop erroring.

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,6 +1,11 @@
-module.exports = {
-    Account: require('./Account.js'),
-    Hunt: require('./Hunt.js'),
-    Item: require('./Item.js'),
-    Submission: require('./Submission.js'),
-};
+// module.exports = {
+//     Account: require('./Account.js'),
+//     Hunt: require('./Hunt.js'),
+//     Item: require('./Item.js'),
+//     Submission: require('./Submission.js'),
+// };
+
+module.exports.Account = require('./Account.js');
+module.exports.Hunt = require('./Hunt.js');
+module.exports.Item = require('./Item.js');
+module.exports.Submission = require('./Submission.js');
